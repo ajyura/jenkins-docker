4edac8b03795f76d6a85881274f09dba79ae146d
ajyura <adam.j.yura@accenture.com>
2017-04-27 14:11:10 -0400

    Update dl_i_plugins.sh

--- dl_i_plugins.sh
+++ dl_i_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ START = 0 ]]; then
+if [[ START=0 ]]; then
     chmod +x /usr/local/bin/install-plugins_rev.sh
     # rm -f /usr/local/bin/install-plugins.sh
     mv /usr/local/bin/install-plugins_rev.sh /tmp/temp/install-plugins.sh
