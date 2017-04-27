6342ed574a04cf4659f387871fc3f480ac4a143a
ajyura <adam.j.yura@accenture.com>
2017-04-27 12:26:04 -0400

    Update dl_i_plugins.sh

--- dl_i_plugins.sh
+++ dl_i_plugins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x /usr/local/bin/install-plugins_rev.sh
-/usr/local/bin/install-plugins.sh $(cat /usr/share/jenkins/ref/plugins.txt | tr '\n' ' ')
+/usr/local/bin/install-plugins_rev.sh $(cat /usr/share/jenkins/ref/plugins.txt | tr '\n' ' ')
