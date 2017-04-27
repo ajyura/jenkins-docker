a809248335e1e6070b4c0e2dc78a63e26d5e49fc
ajyura <adam.j.yura@accenture.com>
2017-04-27 12:42:17 -0400

    Update dl_i_plugins.sh

--- dl_i_plugins.sh
+++ dl_i_plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 chmod +x /usr/local/bin/install-plugins_rev.sh
+echo 'Running the custom DL & I plugins script'
 /usr/local/bin/install-plugins_rev.sh $(cat /usr/share/jenkins/ref/plugins.txt | tr '\n' ' ')
